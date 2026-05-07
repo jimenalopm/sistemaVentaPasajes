50c0f468de3e5ccd4f7e24506598342f8646f801
DavidArtaviaDev <artaviadavid23r@gmail.com>
2026-05-07 00:59:24 -0600

    cambie el modelo usuarios la parte cliente  para poder usar auth con Django y que este maneje contrasena y correo

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,27 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Cliente(models.Model):
+class Cliente(
+    models.Model
+):  # no usamos correo , contrasena y username con Django para poder despues utilizar funciones ya listas de Django
+    user = models.OneToOneField(  # funcion para vincular 1 cliente a 1 user de django
+        User,
+        on_delete=models.CASCADE,
+        related_name="cliente",  # permite despues poder usar request.user.cliente para acceder al perfil
+    )
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     pasaporte = models.CharField(max_length=20, unique=True)
     nacionalidad = models.CharField(max_length=50)
-    correo = models.CharField(max_length=100, unique=True)
-    telefono = models.CharField(
-        max_length=20, null=True, blank=True
-    )  # null=true es para la base de datos y blank=true es para los formularios de django
-    contrasena = models.CharField(max_length=255)  # es mas grande para poder usar hash
+    telefono = models.CharField(max_length=20, null=True, blank=True)
 
     class Meta:
         db_table = "clientes"
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido} ({self.correo})"
+        return f"{self.nombre} {self.apellido} ({self.user.email})"
 
 
 class Tarjeta(models.Model):
     cliente = models.ForeignKey(
-        Cliente, on_delete=models.CASCADE, db_column="id_cliente"
+        Cliente,
+        on_delete=models.CASCADE,
+        db_column="id_cliente",
+        related_name="tarjetas",  # Permite cliente.tarjetas.all() para listar las tarjetas de un cliente.
     )
     numero_tarjeta = models.CharField(max_length=20, unique=True)
     ccv = models.CharField(max_length=4)
