35dd70ac0c2898e56cec54e36bf52f5365015c70
DavidArtaviaDev <artaviadavid23r@gmail.com>
2026-05-06 23:34:10 -0600

    agregar modelo de Tiquetes

--- tiquetes/models.py
+++ tiquetes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from rutas.models import Ruta, Bus, Horario
+from usuarios.models import Cliente, Tarjeta
+# importaciones de las otras tablas para las FKs
 
-# Create your models here.
+
+class Tiquete(models.Model):
+    horario = models.ForeignKey(
+        Horario, on_delete=models.PROTECT, db_column="id_horario"
+    )
+    tarjeta = models.ForeignKey(
+        Tarjeta, on_delete=models.PROTECT, db_column="id_tarjeta"
+    )
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.PROTECT, db_column="id_cliente"
+    )
+    bus = models.ForeignKey(
+        Bus,
+        on_delete=models.PROTECT,
+        to_field="placa",
+        db_column="placa",  # to_field es para decirle directamente cuando no es la id default
+    )
+    codigo = models.CharField(max_length=30, unique=True)
+    fecha_compra = models.DateTimeField(
+        auto_now_add=True
+    )  # auto_now_add es para poner la hora y fecha automaticamente
+    fecha_salida = models.DateTimeField()
+    monto_pagado = models.DecimalField(max_digits=8, decimal_places=2)
+    estado = models.CharField(max_length=20, default="Activo")
+
+    class Meta:
+        db_table = "tiquetes"
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(monto_pagado__gte=0),  # greater than or equal
+                name="chk_tiquetes_monto_positivo",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.codigo} - {self.cliente} ({self.fecha_salida})"
